17f68337f1888569878fd626d9c1fe0f3989bd91
James A. Ware <jamesaware@rocketmail.com>
2022-05-06 23:11:59 +0100

    changes

--- grow_clock.ino
+++ grow_clock.ino
@@ -1,29 +1,72 @@
-
+/*
+ * Grow Clock 
+ * James A Ware 
+ * MAY-22
+ * appologies for the spaghetti
+ * 
+ */
+ 
 #include "Wire.h"
 #include "Servo.h"
-
 #define DS3231_I2C_ADDRESS 0x68
+
+
+const int BLED=11;
+const int GLED=10;
+const int RLED=6;
+int ledMode = 0;
+const long intervalLED = 5000;  
+unsigned long previousMillisLED = 0; 
+
+const int BUTTON=2;
+boolean lastButton = LOW;
+boolean currentButton = LOW;
+
+
+int servo_pin = 9; //D9 on NANO board
+Servo myservo;
+int angle = 0;  
+
+
+//RTC timer 
+const long interval = 1000;  
+unsigned long previousMillis = 0; 
 
 // Convert normal decimal numbers to binary coded decimal
 byte decToBcd(byte val){
   return( (val/10*16) + (val%10) );
 }
+
 // Convert binary coded decimal to normal decimal numbers
 byte bcdToDec(byte val){
   return( (val/16*10) + (val%16) );
 }
-int servo_pin = 9; //D9 on NANO board
-
-Servo myservo;
-int angle = 0;  
 
 void setup(){
+  pinMode (BLED, OUTPUT);
+  pinMode (GLED, OUTPUT);
+  pinMode (RLED, OUTPUT);
+  pinMode (BUTTON, INPUT); 
   Wire.begin();
   Serial.begin(9600);
+ 
+{ 
+  myservo.attach(servo_pin);
+} 
   // set the initial time here:
   // DS3231 seconds, minutes, hours, day, date, month, year
-  setDS3231time(15,4,20,5,5,5,22);
+  //setDS3231time(25,36,21,6,6,5,22);
 }
+
+  boolean debounce(boolean last){
+    boolean current = digitalRead(BUTTON);
+    if(last != current){
+      delay(5);
+      current = digitalRead(BUTTON);
+    }
+    return current;
+  }
+
 void setDS3231time(byte second, byte minute, byte hour, byte dayOfWeek, byte
 dayOfMonth, byte month, byte year){
   // sets time and date data to DS3231
@@ -38,6 +81,7 @@ dayOfMonth, byte month, byte year){
   Wire.write(decToBcd(year)); // set year (0 to 99)
   Wire.endTransmission();
 }
+
 void readDS3231time(byte *second,
 byte *minute,
 byte *hour,
@@ -58,6 +102,7 @@ byte *year){
   *month = bcdToDec(Wire.read());
   *year = bcdToDec(Wire.read());
 }
+
 void displayTime(){
   byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
   // retrieve data from DS3231
@@ -106,31 +151,79 @@ void displayTime(){
     Serial.println("Saturday");
     break;
   }
-
-
-  myservo.attach(servo_pin);
-  
 }
+
+void moveTehArm(){
+  byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
+  // retrieve data from DS3231
+  readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
+  if(hour > 7 && hour < 20) {
+      myservo.write(0);
+      Serial.println(hour);
+      Serial.println("Sun up");
+ 
+  } else {
+    myservo.write(180);
+    Serial.println(hour);
+    Serial.println("Sun down");
+  }
+}
+
+void setMode(int mode){
+  if (mode == 1){
+    digitalWrite(RLED, 64);
+    digitalWrite(GLED, LOW);
+    digitalWrite(BLED, LOW);
+  }
+  else if (mode == 2){
+    digitalWrite(RLED, LOW);
+    digitalWrite(GLED, 64);
+    digitalWrite(BLED, LOW);
+  }
+  else if (mode == 3){
+    digitalWrite(RLED, LOW);
+    digitalWrite(GLED, LOW);
+    digitalWrite(BLED, 64);
+  }
+  else if (mode ==4){
+    digitalWrite(RLED, 32);
+    digitalWrite(GLED, 0);
+    digitalWrite(BLED, 32);
+  }
+  else if (mode == 5){
+    digitalWrite(RLED, 0);
+    digitalWrite(GLED, 32);
+    digitalWrite(BLED, 32);
+  }
+  else if (mode ==6){
+    digitalWrite(RLED, 32);
+    digitalWrite(GLED, 32);
+    digitalWrite(BLED, 0);
+  }
+  else{
+    digitalWrite(RLED, 16);
+    digitalWrite(GLED, 32);
+    digitalWrite(BLED, 16);
+  }
+}
+
 void loop(){
-  //time module
-  displayTime(); // display the real-time clock data on the Serial Monitor,
-  delay(1000); // every second
-
-  //servo module
-  for(angle = 0; angle < 180; angle += 1)
-  {                          
-    myservo.write(angle);
-    delay(15);                       
-  } 
-
-  delay(1000);
   
-  // move from 180 to 0 degrees with a negative angle of 5
-  for(angle = 180; angle>=1; angle-=5)
-  {                                
-    myservo.write(angle);
-    delay(5);                       
-  } 
-
-    delay(1000);
+  unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      displayTime(); 
+      moveTehArm();
+    }
+  unsigned long currentMillisLED = millis();
+  currentButton = debounce(lastButton);
+  if(lastButton == LOW && currentButton == HIGH) {
+    do{
+        ledMode++;
+        if (ledMode ==7) ledMode = 0;
+        setMode(ledMode);
+        delay(1000);
+    }while(currentMillisLED - previousMillisLED >= intervalLED);
+  }
+  lastButton = currentButton;
 }
